3b2444455e4ac83f5686787c7eb47d1c4b99703c
EntesarAbubaker <entesarabubaker78@gmail.com>
2024-11-28 18:23:28 +0300

    DataSructure

---
+++ src/Q15.java
@@ -0,0 +1,16 @@
+public class Q15 {
+    public static void Vowels (String word) {
+        String [] vowels = {"i","a","e","o","u"};
+        if (word == null)
+            System.out.println("The word is null!!!");
+        int count = 0;
+        String str = word.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+
+
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
